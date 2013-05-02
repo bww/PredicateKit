782b94433815d73039d1c3a435e22bb1b665742c
Brian William Wolter <brian@woltergroup.net>
2013-05-02 19:33:18 -0400

    Initial Commit

---
+++ PredicateKitTests/PredicateKitTests.h
@@ -0,0 +1,13 @@
+//
+//  PredicateKitTests.h
+//  PredicateKitTests
+//
+//  Created by Brian William Wolter on 5/2/13.
+//  Copyright (c) 2013 Brian William Wolter. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PredicateKitTests : SenTestCase
+
+@end
---
+++ PredicateKitTests/PredicateKitTests.m
@@ -0,0 +1,32 @@
+//
+//  PredicateKitTests.m
+//  PredicateKitTests
+//
+//  Created by Brian William Wolter on 5/2/13.
+//  Copyright (c) 2013 Brian William Wolter. All rights reserved.
+//
+
+#import "PredicateKitTests.h"
+
+@implementation PredicateKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PredicateKitTests");
+}
+
+@end
