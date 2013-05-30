a36b1872164c68aa400030864e9925a87cb2a02b
Brian William Wolter <brian@woltergroup.net>
2013-05-30 18:20:46 -0400

    ...

---
+++ PredicateKit/source/predicate/util/PKSpan.h
@@ -0,0 +1,17 @@
+// 
+// Copyright (c) 2013 Brian William Wolter. All rights reserved.
+// 
+// @LICENSE@
+// 
+// Developed in New York City
+// 
+
+@class PKExpression;
+
+/**
+ * A text span
+ */
+@interface PKSpan : NSObject
+
+@end
+
---
+++ PredicateKit/source/predicate/util/PKSpan.m
@@ -0,0 +1,14 @@
+// 
+// Copyright (c) 2013 Brian William Wolter. All rights reserved.
+// 
+// @LICENSE@
+// 
+// Developed in New York City
+// 
+
+#import "PKSpan.h"
+
+@implementation PKSpan
+
+@end
+
